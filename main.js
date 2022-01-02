4ff5c2f519093477a0c6440d16bfc91e45f9c79b
Sachin Samal <88958992+Sacsam005@users.noreply.github.com>
2022-01-02 01:05:29 -0600

    edited

---
+++ main.js
@@ -0,0 +1,5 @@
+let arr = [2, 6, 7, 8, 7];
+
+let arr1 = arr.map(x => x % 3 == 0);
+
+console.log(arr1);
--- myPortfolio-website-main/main.js
+++ myPortfolio-website-main/main.js
@@ -1,27 +1,3 @@
-// h4 class="text-center"><b>Hello All</b>👋<h4>
-//             <p class="text-center"><b>I am </b></p>
-
-// const showName = () => {
-//   let name = document.querySelector("#result");
-//   name.innerHTML = "a Front-end Developer";
-
-//   setTimeout(() => {
-//     name.innerHTML = "an Aspirant";
-//   }, 2000);
-
-//   setTimeout(() => {
-//     name.innerHTML = "a Front-end Engineer";
-//   }, 4000);
-
-//   setTimeout(() => {
-//     name.innerHTML = "a Web-Designer";
-//   }, 6000);
-// };
-
-// showName();
-
-// setInterval(showName, 8000);
-
 // Auto typing text effect
 var typed = new Typed(".result", {
   strings: [
